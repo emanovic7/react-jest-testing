b5a59eec4d06cd5ecb579cea28ae6c3f729d567c
emanovic7 <e_owusu@live.com>
2020-09-17 13:53:23 -0400

    tests for page elements

--- counter-app-testing/src/App.js
+++ counter-app-testing/src/App.js
@@ -7,18 +7,22 @@ function App() {
 
   return (
     <div className="App">
-      <div className='header'>
+      <div className='header' data-test='header'>
         <h1>Counter</h1>
       </div>
 
       <div className='buttons'>
-        <span><button>click to increase count</button></span>
+        <span data-test='increase'>
+          <button onClick={() => setCount(count + 1)}>click to increase count</button>
+        </span>
         &nbsp;
-        <span><button>click to decrease count</button></span>
+        <span data-test='decrease'>
+          <button onClick={() => setCount(count - 1)}>click to decrease count</button>
+        </span>
       </div>
 
       <div>
-        <h2>The current count is {count}</h2>
+        <span data-test='counter-display'><h2>The current count is {count}</h2></span>
       </div>
     </div>
   );
--- counter-app-testing/src/App.test.js
+++ counter-app-testing/src/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+//components
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const wrapperSetup = () => shallow(<App />);
+
+const findByAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
+//tests
+test('renders page without error', () => {
+  const wrapper = wrapperSetup();
+  const appHeader = findByAttr(wrapper, 'header');
+  expect(appHeader.length).toBe(1);
+});
+
+test('renders increase button', () => {
+  const wrapper = wrapperSetup();
+  const increaseButton = findByAttr(wrapper, 'increase');
+  expect(increaseButton.length).toBe(1);
+});
+
+test('renders decrease button', () => {
+  const wrapper = wrapperSetup();
+  const decreaseButton = findByAttr(wrapper, 'decrease');
+  expect(decreaseButton.length).toBe(1);
+});
+
+test('renders current counter display', () => {
+  const wrapper = wrapperSetup();
+  const counterDispaly = findByAttr(wrapper, 'counter-display');
+  expect(counterDispaly.length).toBe(1);
 });
